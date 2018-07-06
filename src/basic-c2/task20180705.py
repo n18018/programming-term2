8d6f88016b773b1d6410f185b8a38fc58b8c1c2d
n18018 <n18018@std.it-college.ac.jp>
2018-07-06 09:18:57 +0900

    task20180705の提出

---
+++ src/basic-c2/task20180705.py
@@ -0,0 +1,14 @@
+# 値段
+beer_v = 200
+tumami_v = 100
+yakitori_v = 100 * (1 - 0.2) # 2割引のため
+# 個数
+beer_c = 2
+tumami_c = 1
+yakitori_c = 2
+# 値引き
+nebiki = 150
+# 計算
+sum_v = ((beer_v * beer_c) + (tumami_v * tumami_c) + (yakitori_v * yakitori_c)) - nebiki
+# 結果を表示
+print("会計は", sum_v, "円です")
