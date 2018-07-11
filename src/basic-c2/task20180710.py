e44f1743364cafa14a5f260a8131fd82dc88fd30
n18018 <n18018@std.it-college.ac.jp>
2018-07-11 17:10:01 +0900

    課題の提出

--- src/basic-c2/task20180710.py
+++ src/basic-c2/task20180710.py
@@ -1,16 +1,16 @@
 # 変数
-my_hand = int(input("何を出しますか? 1:グー 2:チョキ 3:パー => "))
+my_hand = input("何を出しますか? 1:グー 2:チョキ 3:パー => ")
 case_1 = "CPUがパーを出しました。あなたの負けです。"
 case_2 = "CPUがグーを出しました。あなたの負けです。"
 case_3 = "CPUがチョキをだしました。あなたの負けです。"
 case_4 = "入力が不正です。終了します。"
 
 # 勝負とその結果
-if my_hand == 1:
+if my_hand == '1':
     print(case_1)
-elif my_hand == 2:
+elif my_hand == '2':
     print(case_2)
-elif my_hand == 3:
+elif my_hand == '3':
     print(case_3)
 else:
     print(case_4)
