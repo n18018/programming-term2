6ade3a6656bfc1f889e60cad0d1d0816d34f9f75
n18018 <n18018@std.it-college.ac.jp>
2018-07-17 17:38:42 +0900

    課題の提出

--- src/basic-c2/task20180712-for-2.py
+++ src/basic-c2/task20180712-for-2.py
@@ -1,7 +1,9 @@
-print("--------------------------")
-for x in range(0, 9):
-    for y in range(0, 9):
-            print('{0}'.format('%2d ' % ((x+1) * (y+1))), end="")
+print('  |  1  2  3  4  5  6  7  8  9')
+print("--+---------------------------")
+for x in range(1, 10):
+    print(str(x), '|', end="")
+    for y in range(1, 10):
+        print("%3d" % (x * y), end="")
     print('')
 
 # 3行目はネットの情報を参考
