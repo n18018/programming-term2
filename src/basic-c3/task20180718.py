33dd96b38fa0b8969f46cdc8893318db1d34123e
n18018 <n18018@std.it-college.ac.jp>
2018-07-31 13:44:57 +0900

    課題の提出

--- src/basic-c3/task20180718.py
+++ src/basic-c3/task20180718.py
@@ -22,11 +22,13 @@ op = {'LowFatMilk': 0,
 # オーダーリスト
 a_order = []
 sum_v = 0
+sum_m = 0
 
 # オーダー開始
 while True:
     order_1 = input("メインメニューを選んでください。")
     if order_1 in main:
+        sum_m = op[order_1]
         print("メインメニューを承りました。")
         break
     elif order_1 == "" or order_1 == "q":
@@ -38,14 +40,19 @@ while True:
 while True:
     order_2 = input("オプションメニューを選んでください。")
     if order_2 in op:
-        a_order = order_2
-        sum_v = order_2
+        a_order.append(order_2)
+        sum_v = op[order_2]
         print("他にオプションメニューの注文はございますか?")
     elif order_2 == "" or order_2 == "q":
         break
     else:
-        print("選択されたオプションはありません。"
+        print("選択されたオプションはありません。\n"
               "他にオプションメニューの注文はございますか?")
 
 # 注文内容の確認
-print("注文内容は[", order_1, a_order, "]です。合計金額は",  "です。右奥のカウンターにてお待ちください。")
+print("注文内容は[", order_1, a_order, "]です。")
+print("合計金額は", sum_v + sum_m, "円です。")
+print("右奥のカウンターにてお待ちください。")
+
+
+# 合計金額の出力がまだです。
