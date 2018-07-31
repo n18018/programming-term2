497e291d882613942d70a038fd1d6fc78f26765a
n18018 <n18018@std.it-college.ac.jp>
2018-07-31 16:51:15 +0900

    課題7/26(lv.2)の提出

---
+++ src/basic-c3/task20180726_lv[2].py
@@ -0,0 +1,18 @@
+import random
+cur_x = 0
+
+while True:
+    if cur_x < 10:
+        shake_dice = random.randint(1, 6)
+        cur_x = cur_x + shake_dice
+        input("サイコロを降ってください")
+        print(shake_dice, "が出ました。", "現在位置は", cur_x, "です。")
+    if cur_x > 10:
+        shake_dice = random.randint(1, 6)
+        cur_x = cur_x - shake_dice
+        print("ゴールを超えているので逆走します。")
+        input("サイコロを降ってください")
+        print(shake_dice, "が出ました。", "現在位置は", cur_x, "です。")
+    if cur_x == 10:
+        print("おめでとうございます、ゴールしました!")
+        break
