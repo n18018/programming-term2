a2e7db7389293191968e21d2f8792dcdb851201e
n18018 <n18018@std.it-college.ac.jp>
2018-07-31 16:26:17 +0900

    授業内容UP

---
+++ src/basic-c3/gen-prime.py
@@ -0,0 +1,17 @@
+# 素数を返すイテレータ
+def genPrime(maxnum):
+    num = 2
+    while (num <= maxnum):
+        is_prime = True  #  素数かどうかを管理する変数を定義
+        for i in range(2, num):  #  numが2の時は範囲に含まれる値がなく実行されない
+            if (num % i) == 0:
+                is_prime = False
+                break
+        if (is_prime): yield num
+        num += 1
+
+# イテレータを得る
+it = genPrime(50)
+# 画面に出力
+for i in it:
+    print(i, end=",")
