43a7394ee4efe793db8cb182ac08c7f09b183420
n18018 <n18018@std.it-college.ac.jp>
2018-08-01 17:08:19 +0900

    現状の課題の提出

--- src/basic-c3/task20180801_q02.py
+++ src/basic-c3/task20180801_q02.py
@@ -1,10 +1,10 @@
 # 金額をリストに格納・変数の作成
 money = [13600, 14500, 16000, 11111, 11667]
-TAX_RATE = 8
+TAX_RATE = 10
 
 # 税率の変数の作成
-t8 = lambda t: round(t * (1 + TAX_RATE / 100))
+t10 = lambda t: round(t * (1 + TAX_RATE / 100))
 
-print(list(map(t8, money)))
+print(list(map(t10, money)))
 
 # 現状
---
+++ src/basic-c3/task20180801_q04.py
@@ -0,0 +1,7 @@
+r1 = range(1, 41)
+
+print(list(filter(lambda x: (x % 10) == 3, r1)))
+print(list(filter(lambda x: (x // 10) == 3, r1)))
+print(list(filter(lambda x: (x // 10) == 3, r1)))
+
+# 未完成
