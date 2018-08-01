d18972504aa8d7da1757225dc14edcf748a4f812
n18018 <n18018@std.it-college.ac.jp>
2018-08-01 16:11:41 +0900

    課題の名前の修正

---
+++ src/basic-c3/task20180801_q01.py
@@ -0,0 +1,9 @@
+# 金額をリストに格納・変数の作成
+money = [13600, 14500, 16000, 11111, 11667]
+TAX_RATE = 8
+
+print(money)
+# 税率の変数の作成
+# t8 = lambda t: (1 + TAX_RATE / 100)
+
+# print(list(map(lambda t: (1 + TAX_RATE / 100), money)))
