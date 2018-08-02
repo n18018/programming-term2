603bdf52c56e0bc45bf2e6489b1a9bd6c2cc4f3e
n18018 <n18018@std.it-college.ac.jp>
2018-08-02 12:27:45 +0900

    課題の提出

--- src/basic-c3/task20180801_q04.py
+++ src/basic-c3/task20180801_q04.py
@@ -1,7 +1,7 @@
 r1 = range(1, 41)
 
-print(list(filter(lambda x: (x % 10) == 3, r1)))
-print(list(filter(lambda x: (x // 10) == 3, r1)))
-print(list(filter(lambda x: (x // 10) == 3, r1)))
+print(list(filter(lambda x: (x % 3) == 0 or
+                  (x // 10) == 3 or (x % 10) == 3, r1)))
 
-# 未完成
+
+# line基準を超えないよう改行してあります
