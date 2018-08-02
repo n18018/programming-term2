edf2857368573a451ad78c457c17571b91766dd9
n18018 <n18018@std.it-college.ac.jp>
2018-08-02 14:48:13 +0900

    授業課題の提出

---
+++ src/algo-p1/task20180801_q01.py
@@ -0,0 +1,12 @@
+# 変数を使ってみよう
+# 変数greetingに文字列「こんにちは」を代入してください
+greeting = 'こんにちは'
+
+# 変数greetingの値を出力してください
+print(greeting)
+
+# 変数numberに数値の7を代入してください
+number = 7
+
+# 変数numberの値を出力してください
+print(number)
