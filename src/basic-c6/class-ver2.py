9e8b151896095fccf95c1fd87be892b8501e34f4
n18018 <n18018@std.it-college.ac.jp>
2018-08-21 19:42:06 +0900

    授業内容の提出

---
+++ src/basic-c6/class-ver2.py
@@ -0,0 +1,16 @@
+class Cat:
+    # クラス変数
+    nakigoe = "nya-"
+
+    # メソッド
+    def naku(self):
+        print(self.nakigoe)
+
+
+mike = Cat()
+nora = Cat()
+
+# 鳴き声を変更
+mike.nakigoe = "myu-"
+mike.naku()
+nora.naku()
---
+++ src/basic-c6/human-name.py
@@ -0,0 +1,21 @@
+# クラスの定義
+class HumanName:
+    '''人を表すクラス'''
+    def setName(self, name):
+        '''名前を設定するメソッド'''
+        self.name = name
+
+    def getName(self):
+        '''名前を取得するメソッド'''
+        return self.name
+
+
+# インスタンスを生成
+taro = HumanName()
+taro.setName("taro")
+print(taro.getName())
+
+jiro = HumanName()
+jiro.setName("jiro")
+print(jiro.getName())
+print(jiro.name)
